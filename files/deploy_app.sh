dadbd9a55ec3c49178cede1efcd049975317cc97
seraphineyoung <seraphine.young@hashicorp.com>
2024-01-31 13:35:08 +0000

    Updated HTML payload

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,7 +13,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. I love looking at cats but wont get one.
   <!-- END -->
 
   </div>
