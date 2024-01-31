5a12c93686e3b93831f32742e094e77facd5f253
seraphineyoung <seraphine.young@hashicorp.com>
2024-01-31 14:27:43 +0000

    Added s3 bucket private module

--- main.tf
+++ main.tf
@@ -202,5 +202,6 @@ resource "aws_key_pair" "hashicat" {
 module "s3-bucket" {
   source              = "cloudposse/s3-bucket/aws"
   version             = "3.1.0"
+  tags                =  "1.0.0"
   s3_object_ownership = "BucketOwnerEnforced"
 }
